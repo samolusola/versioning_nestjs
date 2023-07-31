5b732b965948c53b5a11889457a282f44ba34476
Samuel Olusola <solathecoder@gmail.com>
2023-07-31 12:21:09 +0100

    🔥 Version: Product v2

---
+++ src/product/product.controller-v2.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { ApiTags } from '@nestjs/swagger';
+
+@Controller({
+  path: 'products',
+  version: '2',
+})
+@ApiTags('products')
+export class ProductControllerV2 {
+  constructor(private readonly productService: ProductService) {}
+
+  @Get()
+  getAllProducts() {
+    return this.productService.getAllProducts();
+  }
+
+  @Post()
+  createProduct(@Body() createProductDto: CreateProductDto) {
+    return this.productService.createProduct(createProductDto);
+  }
+}
--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
+import { ProductControllerV2 } from './product.controller-v2';
 
 @Module({
   providers: [ProductService],
-  controllers: [ProductController],
+  controllers: [ProductController, ProductControllerV2],
 })
 export class ProductModule {}
