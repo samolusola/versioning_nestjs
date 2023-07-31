f8b7383b4d358a4f880b3cf9071e698ce2e5d707
Samuel Olusola <solathecoder@gmail.com>
2023-07-31 12:01:16 +0100

    🔥 Versioning step 3: Swagger docs

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
     .addTag('commerce')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup(`${API_PREFIX}/:version/docs`, app, document);
 
   await app.listen(3030);
 }
